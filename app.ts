12e4b43e02c619c3030dc5028c5620be4f250bf4
Preston Wallace <wallace.preston@gmail.com>
2023-04-13 17:30:55 -0700

    add type definitions and initialize server

--- app.ts
+++ app.ts
@@ -1,2 +1,13 @@
 // Import express
 import express from 'express';
+
+// Create a new express app instance
+const app = express();
+
+// Make the PORT configurable
+const { PORT = 3000 } = process.env;
+
+// Start the Express server, displaying the localhost URL
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
